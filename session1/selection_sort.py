e5fa152abebf26c27ae6f05d4facf5bea33690e0
gpt0507 <gauravagrawal7002@gmail.com>
2025-05-16 08:13:37 +0530

    update all

--- session1/selection_sort.py
+++ session1/selection_sort.py
@@ -1,3 +1,21 @@
+# # # way1
+# def selection_sort(arr):
+#   for i in range(len(arr)-1):
+#     min=arr[i]
+#     for j in range(i+1,len(arr)):
+#         if arr[j]<min:
+#           min=arr[j]
+#           loc=j
+#     temp=arr[i]      
+#     arr[i]=min
+#     arr[loc]=temp
+#   return arr
+
+
+# print(selection_sort([7,3,18,17]))
+
+
+# # way2
 def selection_sort(arr):
   for i in range(len(arr)-1):
     min=arr[i]
@@ -5,10 +23,20 @@ def selection_sort(arr):
         if arr[j]<min:
           min=arr[j]
           loc=j
-    temp=arr[i]      
-    arr[i]=min
-    arr[loc]=temp
+    arr[i],arr[loc]=arr[loc],arr[i]
   return arr
 
-
 print(selection_sort([7,3,18,17]))
+
+# # # Way3
+# def selection_sort(arr):
+#   for i in range(len(arr)-1):
+#     min=i
+#     for j in range(i+1,len(arr)):
+#         if arr[min]>arr[j]:
+#           min=j
+#     arr[min],arr[i]=arr[i],arr[min]
+#   return arr
+
+
+# # print(selection_sort([7,3,18,17]))
